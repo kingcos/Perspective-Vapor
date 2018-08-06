6ea46fa4a075e47e4c56c8cf2a445ea0c69c4fc5
kingcos <2821836721v@gmail.com>
2018-08-06 23:51:44 +0800

    Add SwiftSoup

--- Package.swift
+++ Package.swift
@@ -8,10 +8,13 @@ let package = Package(
         .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0"),
 
         // 🔵 Swift ORM (queries, models, relations, etc) built on SQLite 3.
-        .package(url: "https://github.com/vapor/fluent-sqlite.git", from: "3.0.0")
+        .package(url: "https://github.com/vapor/fluent-sqlite.git", from: "3.0.0"),
+        
+        // SwiftSoup
+        .package(url: "https://github.com/scinfu/SwiftSoup.git", from: "1.7.2"),
     ],
     targets: [
-        .target(name: "App", dependencies: ["FluentSQLite", "Vapor"]),
+        .target(name: "App", dependencies: ["FluentSQLite", "Vapor", "SwiftSoup"]),
         .target(name: "Run", dependencies: ["App"]),
         .testTarget(name: "AppTests", dependencies: ["App"])
     ]
