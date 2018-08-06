2777efee912a957dc862ab5608ba325d56b1caf7
kingcos <2821836721v@gmail.com>
2018-08-06 23:52:25 +0800

    Add GraphQL

--- Package.swift
+++ Package.swift
@@ -12,9 +12,12 @@ let package = Package(
         
         // SwiftSoup
         .package(url: "https://github.com/scinfu/SwiftSoup.git", from: "1.7.2"),
+        
+        // GraphQL
+        .package(url: "https://github.com/GraphQLSwift/GraphQL.git", from: "0.0.0"),
     ],
     targets: [
-        .target(name: "App", dependencies: ["FluentSQLite", "Vapor", "SwiftSoup"]),
+        .target(name: "App", dependencies: ["FluentSQLite", "Vapor", "SwiftSoup", "GraphQL"]),
         .target(name: "Run", dependencies: ["App"]),
         .testTarget(name: "AppTests", dependencies: ["App"])
     ]
