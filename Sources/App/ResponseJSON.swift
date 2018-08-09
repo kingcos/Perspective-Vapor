c0c33d08c31e037ea624f38ebb140668f196a596
kingcos <2821836721v@gmail.com>
2018-08-10 01:35:13 +0800

    Add response JSON

---
+++ Sources/App/ResponseJSON.swift
@@ -0,0 +1,46 @@
+//
+//  ResponseJSON.swift
+//  App
+//
+//  Created by kingcos on 2018/8/8.
+//
+
+import Foundation
+import Vapor
+
+struct Empty: Content { }
+
+enum ResponseStatus: Int, Content {
+    case ok = 0
+    case error = -1
+    
+    var description: String {
+        switch self {
+        case .ok:
+            return "请求成功"
+        case .error:
+            return "请求失败"
+        }
+    }
+}
+
+struct ResponseJSON<T: Content> : Content {
+    private var status: ResponseStatus
+    private var message: String
+    private var data: T?
+    
+    init(status: ResponseStatus = .ok,
+         message: String = ResponseStatus.ok.description) {
+        self.status = status
+        self.message = message
+        self.data = nil
+    }
+    
+    init(status: ResponseStatus = .ok,
+         message: String = ResponseStatus.ok.description,
+         data: T?) {
+        self.status = status
+        self.message = message
+        self.data = data
+    }
+}
