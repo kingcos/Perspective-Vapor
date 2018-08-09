4effd9c74c4ae62c34bd0ff59ad72d8b9f09a0e2
kingcos <2821836721v@gmail.com>
2018-08-10 00:34:33 +0800

    Removed GraphQL for lack of exp

--- Package.swift
+++ Package.swift
@@ -4,26 +4,19 @@ import PackageDescription
 let package = Package(
     name: "Perspective-Vapor",
     dependencies: [
-        // 💧 A server-side Swift web framework.
+        // Vapor
         .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0"),
 
-        // 🔵 Swift ORM (queries, models, relations, etc) built on SQLite 3.
+        // SQLite 3
         .package(url: "https://github.com/vapor/fluent-sqlite.git", from: "3.0.0"),
         
         // SwiftSoup
         .package(url: "https://github.com/scinfu/SwiftSoup.git", from: "1.7.2"),
-        
-        // GraphQL
-        .package(url: "https://github.com/GraphQLSwift/GraphQL.git", from: "0.0.0"),
-        // Apollo
-        .package(url: "https://github.com/apollographql/apollo-ios.git", from: "0.9.2")
     ],
     targets: [
         .target(name: "App", dependencies: ["FluentSQLite",
                                             "Vapor",
-                                            "SwiftSoup",
-                                            "GraphQL",
-                                            "Apollo"]),
+                                            "SwiftSoup"]),
         .target(name: "Run", dependencies: ["App"]),
         .testTarget(name: "AppTests", dependencies: ["App"])
     ]
