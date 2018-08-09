43a5e74477a2d1d61551700a50848617aa9725bc
kingcos <2821836721v@gmail.com>
2018-08-10 01:34:43 +0800

    Add HTTPBody extention

---
+++ Sources/App/Util/Extension/HTTPBody+Extension.swift
@@ -0,0 +1,14 @@
+//
+//  HTTPBody+Extension.swift
+//  App
+//
+//  Created by kingcos on 2018/8/10.
+//
+
+import Vapor
+
+extension HTTPBody {
+    var utf8String: String {
+        return String(data: data ?? Data(), encoding: .utf8) ?? "n/a"
+    }
+}
