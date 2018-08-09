7bad2ed47b26911cc74d8a9b6ae54bc0a6b04189
kingcos <2821836721v@gmail.com>
2018-08-10 01:35:45 +0800

    Support fetchLabels from GitHub API

---
+++ Sources/App/Controllers/PerspectiveController.swift
@@ -0,0 +1,62 @@
+//
+//  PerspectiveController.swift
+//  App
+//
+//  Created by kingcos on 2018/8/7.
+//
+
+import Foundation
+import Vapor
+import Apollo
+
+class PerspectiveController: RouteCollection {
+    
+    var timer: Scheduled<()>?
+    
+    var labels: [Label] = []
+    
+    func boot(router: Router) throws {
+        router.group("github") { router in
+            router.get("start", use: start)
+//            router.get("labels", use: fetchLabels)
+//            router.get("cancel", use: )
+//            router.get("cancel", use: )
+        }
+    }
+    
+    
+}
+
+extension PerspectiveController {
+    func start(_ request: Request) throws -> Future<Response> {
+        guard timer == nil else {
+            return try ResponseJSON<Empty>(status: .error, message: "Timer is running.").encode(for: request)
+        }
+        
+        // Do sth...
+        _ = try fetchLables(request)
+        
+        return try ResponseJSON<Empty>(status: .ok, message: "Starting...").encode(for: request)
+    }
+}
+
+extension PerspectiveController {
+    func fetchLables(_ request: Request) throws -> Future<Response> {
+        guard let apiURL = (Constants.GitHubAPIPrefix + Constants.GitHubAPI.Labels).convertToURL() else {
+            return try ResponseJSON<Empty>(status: .error, message: "URL error.").encode(for: request)
+        }
+        
+        let authHeader: HTTPHeaders = ["Authorization" : "token \(Constants.GitHubAPIToken)"]
+        let apiRequest = HTTPRequest(method: .GET, url: apiURL, headers: authHeader)
+        let getRequest = Request(http: apiRequest, using: request)
+        
+        return try request.client().send(getRequest).flatMap(to: Response.self) { response in
+            let json = response.http.body.utf8String
+            let data = json.convertToData()
+            let decoder = JSONDecoder()
+            let item = try decoder.decode([Label].self, from: data)
+            print(item.count)
+            return try ResponseJSON<Empty>(status: .ok, message: "Starting...").encode(for: request)
+        }
+    }
+}
