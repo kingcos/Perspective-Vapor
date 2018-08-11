1bf8813184a12a719f32f5f92a91a71833f77bd2
kingcos <2821836721v@gmail.com>
2018-08-11 23:54:15 +0800

    Add SQL config

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -1,11 +1,10 @@
-import FluentSQLite
+import FluentPostgreSQL
 import Vapor
 
 /// Called before your application initializes.
-public func configure(_ config: inout Config, _ env: inout Environment, _ services: inout Services) throws {
-    /// Register providers first
-    try services.register(FluentSQLiteProvider())
-
+public func configure(_ config: inout Config,
+                      _ env: inout Environment,
+                      _ services: inout Services) throws {
     /// Register routes to the router
     let router = EngineRouter.default()
     try routes(router)
@@ -16,18 +15,36 @@ public func configure(_ config: inout Config, _ env: inout Environment, _ servic
     /// middlewares.use(FileMiddleware.self) // Serves files from `Public/` directory
     middlewares.use(ErrorMiddleware.self) // Catches errors and converts to HTTP response
     services.register(middlewares)
-
-    // Configure a SQLite database
-    let sqlite = try SQLiteDatabase(storage: .memory)
-
-    /// Register the configured SQLite database to the database config.
+    
+    // -- SQL --
+    // 注册 PostgreSQL
+    try services.register(FluentPostgreSQLProvider())
+    let postgreSQLDatabase = PostgreSQLDatabase(config: .PerspectiveConfig)
+    
+    // 将 PostgreSQL 注册到数据库配置中
     var databases = DatabasesConfig()
-    databases.add(database: sqlite, as: .sqlite)
+    databases.add(database: postgreSQLDatabase, as: .psql)
     services.register(databases)
 
     /// Configure migrations
     var migrations = MigrationConfig()
     migrations.add(model: Todo.self, database: .sqlite)
     services.register(migrations)
+}
 
+// MARK: SQL Config
+extension PostgreSQLDatabaseConfig {
+    static let PerspectiveConfig: PostgreSQLDatabaseConfig = {
+        let hostname = "http://localhost"
+        let port = 5432
+        let username = "kingcos"
+        let database = "Perspective"
+        let password = ""
+        
+        return PostgreSQLDatabaseConfig(hostname: hostname,
+                                        port: port,
+                                        username: username,
+                                        database: database,
+                                        password: password)
+    }()
 }
