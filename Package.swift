cb708f834dbb48ba3bf03d2d63760ce87a04b90d
kingcos <2821836721v@gmail.com>
2018-08-11 13:57:39 +0800

    Replaced SQLite with PostgreSQL

--- Package.swift
+++ Package.swift
@@ -7,15 +7,15 @@ let package = Package(
         // Vapor
         .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0"),
 
-        // SQLite 3
-        .package(url: "https://github.com/vapor/fluent-sqlite.git", from: "3.0.0"),
+        // PostgreSQL
+        .package(url: "https://github.com/vapor/fluent-postgresql.git", from: "1.0.0"),
         
         // SwiftSoup
         .package(url: "https://github.com/scinfu/SwiftSoup.git", from: "1.7.2"),
     ],
     targets: [
-        .target(name: "App", dependencies: ["FluentSQLite",
-                                            "Vapor",
+        .target(name: "App", dependencies: ["Vapor",
+                                            "FluentPostgreSQL",
                                             "SwiftSoup"]),
         .target(name: "Run", dependencies: ["App"]),
         .testTarget(name: "AppTests", dependencies: ["App"])
