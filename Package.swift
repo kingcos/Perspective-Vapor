ea4551683c62aeca549ed8dba3fce3e153a7b7d6
kingcos <2821836721v@gmail.com>
2018-08-13 01:16:00 +0800

    Removed all ORMs

--- Package.swift
+++ Package.swift
@@ -5,18 +5,10 @@ let package = Package(
     name: "Perspective-Vapor",
     dependencies: [
         // Vapor
-        .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0"),
-
-        // PostgreSQL
-        .package(url: "https://github.com/vapor/fluent-postgresql.git", from: "1.0.0"),
-        
-        // SwiftSoup
-        .package(url: "https://github.com/scinfu/SwiftSoup.git", from: "1.7.2"),
+        .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0")
     ],
     targets: [
-        .target(name: "App", dependencies: ["Vapor",
-                                            "FluentPostgreSQL",
-                                            "SwiftSoup"]),
+        .target(name: "App", dependencies: ["Vapor"),
         .target(name: "Run", dependencies: ["App"]),
         .testTarget(name: "AppTests", dependencies: ["App"])
     ]
