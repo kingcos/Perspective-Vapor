056fdad9f2ffc5d59b06d25d7d2924ec635d79f5
kingcos <2821836721v@gmail.com>
2018-08-13 17:42:45 +0800

    Fixed

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let package = Package(
         .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0")
     ],
     targets: [
-        .target(name: "App", dependencies: ["Vapor"),
+        .target(name: "App", dependencies: ["Vapor"]),
         .target(name: "Run", dependencies: ["App"]),
         .testTarget(name: "AppTests", dependencies: ["App"])
     ]
