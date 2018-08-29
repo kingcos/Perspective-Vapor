a45eb322c9f2d693bc474f1f17a556b945f5bffc
kingcos <2821836721v@gmail.com>
2018-08-29 10:05:11 +0800

    Now support more generic repo issues

--- Sources/App/Controllers/PerspectiveController.swift
+++ Sources/App/Controllers/PerspectiveController.swift
@@ -15,70 +15,76 @@ class PerspectiveController: RouteCollection {
     var labels: [Label] = []
     
     func boot(router: Router) throws {
-        router.group("perspective") { router in
-            router.get("labels", use: fetchLables)
-            router.get("issues", use: fetchIssues)
-            router.get("comments", use: fetchComments)
+        router.group("github") { router in
+            router.get(String.parameter, String.parameter, "issues", use: fetchIssuesList)
         }
     }
 }
 
 extension PerspectiveController {
-    func fetchLables(_ request: Request) throws -> Future<Response> {
-        guard let apiURL = (Constants.GitHubAPIPrefix + Constants.GitHubAPI.Labels).convertToURL() else {
+//    func fetchLables(_ request: Request) throws -> Future<Response> {
+//        guard let apiURL = (Constants.GitHubAPIPrefix + Constants.GitHubAPI.Labels).convertToURL() else {
+//            return try ResponseJSON<Empty>(status: .error,
+//                                           message: "API URL has broken.").encode(for: request)
+//        }
+//
+//        let authHeader: HTTPHeaders = ["Authorization" : "token \(Constants.GitHubAPIToken)"]
+//        let apiRequest = HTTPRequest(method: .GET, url: apiURL, headers: authHeader)
+//        let getRequest = Request(http: apiRequest, using: request)
+//
+//        return try request.client().send(getRequest).flatMap(to: Response.self) { response in
+//            let data = response.http.body.utf8.convertToData()
+//            let items = try JSONDecoder().decode([Label].self, from: data)
+//
+//            return try ResponseJSON<[Label]>(status: .ok,
+//                                             data: items).encode(for: request)
+//        }
+//    }
+    
+    func fetchIssuesList(_ request: Request) throws -> Future<Response> {
+        let username = try request.parameters.next(String.self)
+        let repoName = try request.parameters.next(String.self)
+        let page = request.query[String.self, at: "page"] ?? "1"
+        let perPage = request.query[String.self, at: "per_page"] ?? "10"
+        
+        let url = "\(API.GitHub.Prefix)\(API.GitHub.Repos)/\(username)/\(repoName)\(API.GitHub.Issues)?page=\(page)&per_page=\(perPage)"
+        guard let apiURL = url.convertToURL() else {
             return try ResponseJSON<Empty>(status: .error,
                                            message: "API URL has broken.").encode(for: request)
         }
         
-        let authHeader: HTTPHeaders = ["Authorization" : "token \(Constants.GitHubAPIToken)"]
-        let apiRequest = HTTPRequest(method: .GET, url: apiURL, headers: authHeader)
+        let header: HTTPHeaders = [
+            "Authorization" : "token \(API.GitHub.Token)"
+        ]
+        let apiRequest = HTTPRequest(method: .GET, url: apiURL, headers: header)
         let getRequest = Request(http: apiRequest, using: request)
         
         return try request.client().send(getRequest).flatMap(to: Response.self) { response in
             let data = response.http.body.utf8.convertToData()
-            let items = try JSONDecoder().decode([Label].self, from: data)
+            let items = try JSONDecoder().decode([IssueInfo].self, from: data)
             
-            return try ResponseJSON<[Label]>(status: .ok,
-                                             data: items).encode(for: request)
+            return try ResponseJSON<[IssueInfo]>(status: .ok,
+                                                 data: items).encode(for: request)
         }
     }
     
-    func fetchIssues(_ request: Request) throws -> Future<Response> {
-        guard let apiURL = (Constants.GitHubAPIPrefix + Constants.GitHubAPI.Issues).convertToURL() else {
-            return try ResponseJSON<Empty>(status: .error,
-                                           message: "API URL has broken.").encode(for: request)
-        }
-        
-        let authHeader: HTTPHeaders = ["Authorization" : "token \(Constants.GitHubAPIToken)"]
-        let apiRequest = HTTPRequest(method: .GET, url: apiURL, headers: authHeader)
-        let getRequest = Request(http: apiRequest, using: request)
-        
-        return try request.client().send(getRequest).flatMap(to: Response.self) { response in
-            let data = response.http.body.utf8.convertToData()
-            let items = try JSONDecoder().decode([Issue].self, from: data)
-            
-            return try ResponseJSON<[Issue]>(status: .ok,
-                                             data: items).encode(for: request)
-        }
-    }
-    
-    func fetchComments(_ request: Request) throws -> Future<Response> {
-        guard let apiURL = (Constants.GitHubAPIPrefix + Constants.GitHubAPI.Comments).convertToURL() else {
-            return try ResponseJSON<Empty>(status: .error,
-                                           message: "API URL has broken.").encode(for: request)
-        }
-        
-        let authHeader: HTTPHeaders = ["Authorization" : "token \(Constants.GitHubAPIToken)"]
-        let apiRequest = HTTPRequest(method: .GET, url: apiURL, headers: authHeader)
-        let getRequest = Request(http: apiRequest, using: request)
-        
-        return try request.client().send(getRequest).flatMap(to: Response.self) { response in
-            let data = response.http.body.utf8.convertToData()
-            let decoder = JSONDecoder()
-            let items = try decoder.decode([Comment].self, from: data)
-            
-            return try ResponseJSON<[Comment]>(status: .ok,
-                                               data: items).encode(for: request)
-        }
-    }
+//    func fetchComments(_ request: Request) throws -> Future<Response> {
+//        guard let apiURL = (Constants.GitHubAPIPrefix + Constants.GitHubAPI.Comments).convertToURL() else {
+//            return try ResponseJSON<Empty>(status: .error,
+//                                           message: "API URL has broken.").encode(for: request)
+//        }
+//        
+//        let authHeader: HTTPHeaders = ["Authorization" : "token \(Constants.GitHubAPIToken)"]
+//        let apiRequest = HTTPRequest(method: .GET, url: apiURL, headers: authHeader)
+//        let getRequest = Request(http: apiRequest, using: request)
+//        
+//        return try request.client().send(getRequest).flatMap(to: Response.self) { response in
+//            let data = response.http.body.utf8.convertToData()
+//            let decoder = JSONDecoder()
+//            let items = try decoder.decode([Comment].self, from: data)
+//            
+//            return try ResponseJSON<[Comment]>(status: .ok,
+//                                               data: items).encode(for: request)
+//        }
+//    }
 }
