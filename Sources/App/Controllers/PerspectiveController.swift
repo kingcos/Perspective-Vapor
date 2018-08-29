6b5391fb597f7adebea42aa332c64b89f31a6aed
kingcos <2821836721v@gmail.com>
2018-08-30 00:53:24 +0800

    Fetch one issue details OK

--- Sources/App/Controllers/PerspectiveController.swift
+++ Sources/App/Controllers/PerspectiveController.swift
@@ -17,6 +17,7 @@ class PerspectiveController: RouteCollection {
     func boot(router: Router) throws {
         router.group("github") { router in
             router.get(String.parameter, String.parameter, "issues", use: fetchIssuesList)
+            router.get(String.parameter, String.parameter, "issues", Int.parameter, use: fetchIssueDetails)
         }
     }
 }
@@ -42,12 +43,14 @@ extension PerspectiveController {
 //    }
     
     func fetchIssuesList(_ request: Request) throws -> Future<Response> {
-        let username = try request.parameters.next(String.self)
-        let repoName = try request.parameters.next(String.self)
+        let owner = try request.parameters.next(String.self)
+        let repo = try request.parameters.next(String.self)
+
         let page = request.query[String.self, at: "page"] ?? "1"
         let perPage = request.query[String.self, at: "per_page"] ?? "10"
         
-        let url = "\(API.GitHub.Prefix)\(API.GitHub.Repos)/\(username)/\(repoName)\(API.GitHub.Issues)?page=\(page)&per_page=\(perPage)"
+        let url = "\(API.GitHub.Prefix)\(API.GitHub.Repos)/\(owner)/\(repo)\(API.GitHub.Issues)?page=\(page)&per_page=\(perPage)"
+        
         guard let apiURL = url.convertToURL() else {
             return try ResponseJSON<Empty>(status: .error,
                                            message: "API URL has broken.").encode(for: request)
@@ -65,6 +68,33 @@ extension PerspectiveController {
             
             return try ResponseJSON<[IssueInfo]>(status: .ok,
                                                  data: items).encode(for: request)
+        }
+    }
+    
+    func fetchIssueDetails(_ request: Request) throws -> Future<Response> {
+        let owner = try request.parameters.next(String.self)
+        let repo = try request.parameters.next(String.self)
+        let number = try request.parameters.next(Int.self)
+        
+        let url = "\(API.GitHub.Prefix)\(API.GitHub.Repos)/\(owner)/\(repo)\(API.GitHub.Issues)/\(number)"
+        
+        guard let apiURL = url.convertToURL() else {
+            return try ResponseJSON<Empty>(status: .error,
+                                           message: "API URL has broken.").encode(for: request)
+        }
+        
+        let header: HTTPHeaders = [
+            "Authorization" : "token \(API.GitHub.Token)"
+        ]
+        let apiRequest = HTTPRequest(method: .GET, url: apiURL, headers: header)
+        let getRequest = Request(http: apiRequest, using: request)
+        
+        return try request.client().send(getRequest).flatMap(to: Response.self) { response in
+            let data = response.http.body.utf8.convertToData()
+            let items = try JSONDecoder().decode(IssueDetails.self, from: data)
+            
+            return try ResponseJSON<IssueDetails>(status: .ok,
+                                                  data: items).encode(for: request)
         }
     }
     
