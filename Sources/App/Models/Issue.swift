4397610a7cc6374782ef66373770b87a74e059a7
kingcos <2821836721v@gmail.com>
2018-08-29 10:04:51 +0800

    Add new issue models

--- Sources/App/Models/Issue.swift
+++ Sources/App/Models/Issue.swift
@@ -8,8 +8,15 @@
 import Foundation
 import Vapor
 
-struct Issue: Content {
+struct IssueInfo: Content {
     var number: Int?
+    var title: String?
+    var labels: [Label]?
+    var comments: Int?
+    var created_at: String?
+}
+
+struct IssueDetails: Content {
     var title: String?
     var labels: [Label]?
     var comments: Int?
